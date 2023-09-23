f8d113124d355a51dc463af19cdb0fd76c637a3a
dilldylanpickle <dgonzales4055@gmail.com>
2023-09-23 18:38:45 -0500

    Problem 1 solved

---
+++ problem1.c
@@ -0,0 +1,16 @@
+// Write a C program to create, initialize and demonstrate the use of pointers.
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 69;
+    int *ptr = &i;
+
+    printf("The value of the integer is %d\n", i);
+    printf("The address of the integer is %p\n", &i);
+    printf("The value of the pointer is %p\n", ptr);
+    printf("The dereference of the pointer is %d\n", *ptr);
+
+    return 0;
+}
