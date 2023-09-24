ffcc303f00d78cf17a49cb36d387121142570e26
dilldylanpickle <dgonzales4055@gmail.com>
2023-09-24 15:22:44 -0500

    I cannot think of a good commit message LMAO

--- Dissassembly-Patterns/single-branch-condition.c
+++ Dissassembly-Patterns/single-branch-condition.c
@@ -20,29 +20,33 @@ int main(int argc, char *argv[])
   4004e7:       55                      push   rbp
   4004e8:       48 89 e5                mov    rbp,rsp
 
-  ***** ALLOCATE ROOM ON THE STACK
+  ***** ALLOCATE 0x20 BYTES ON THE STACK
   4004eb:       48 83 ec 20             sub    rsp,0x20
 
   ***** STORE FUNCTION PARAMETERS ON THE STACK
   4004ef:       89 7d ec                mov    DWORD PTR [rbp-0x14],edi
   4004f2:       48 89 75 e0             mov    QWORD PTR [rbp-0x20],rsi
 
-  ***** int i = 69;
+  ***** SET LOCAL VARIABLE "i" TO "69"
   4004f6:       c7 45 fc 45 00 00 00    mov    DWORD PTR [rbp-0x4],0x45
 
-  ***** if (i == 69) {
+  ***** CONDITIONAL TO SEE IF "i" IS EQUAL TO "69":
   4004fd:       83 7d fc 45             cmp    DWORD PTR [rbp-0x4],0x45
   400501:       75 16                   jne    400519 <main+0x32>
+
+  ***** CONDITION TRUE:
   400503:       8b 45 fc                mov    eax,DWORD PTR [rbp-0x4]
   400506:       89 c6                   mov    esi,eax
   400508:       48 8d 3d 95 00 00 00    lea    rdi,[rip+0x95]        # 4005a4 <_IO_stdin_used+0x4>
   40050f:       b8 00 00 00 00          mov    eax,0x0
+
+  ***** CALL PRINTF();
   400514:       e8 d7 fe ff ff          call   4003f0 <printf@plt>
 
-  ***** return 0;
+  ***** RETURN 0; FOR MAIN FUNCTION:
   400519:       b8 00 00 00 00          mov    eax,0x0
 
-  ***** FUNCTION EPILOGUE
+  ***** FUNCTION EPILOGUE:
   40051e:       c9                      leave
   40051f:       c3                      ret
 */
